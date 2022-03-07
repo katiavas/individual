0ff8fcf9027001f78d4ba39c1cd91f8a75fa44bb
katiavas <bl19548@bristol.ac.uk>
2022-03-07 11:40:02 +0000

    test

--- worker.py
+++ worker.py
@@ -96,7 +96,7 @@ def worker(name, input_shape, n_actions, global_agent, global_icm,
             input_img = env.render(mode='rgb_array')
             input_img = resize(input_img, (3, 240, 160))
             # input_img = get_image(env)
-            # print("input img worker render", input_img.shape)
+            print("input img worker render", input_img.shape)
             state = T.tensor([input_img], dtype=T.float)
             print("state/input img shape in worker", state.shape)
             # input_img = get_screen(env)

02f8f10aa92ebc9ff13cb37af9ded2928ab73d2b
katiavas <bl19548@bristol.ac.uk>
2022-03-07 18:49:03 +0000

    test

--- icm.py
+++ icm.py
@@ -96,6 +96,7 @@ class ICM(nn.Module):
         return pi_logits, state_
 
     def calc_loss(self, state, new_state, action):
+        print(state.shape)
         state = T.tensor(state, dtype=T.float)
         action = T.tensor(action, dtype=T.float)
         new_state = T.tensor(new_state, dtype=T.float)

aad64da1912013b066c0a30284fff1702a9c69aa
katiavas <bl19548@bristol.ac.uk>
2022-03-08 19:57:31 +0000

    test

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
     torch.manual_seed(5)
     mp.set_start_method('spawn')
     memory = Memory()
-    # env_id = 'ALE/Breakout-v5'
-    env_id = 'CartPole-v1'
+    env_id = 'ALE/Breakout-v5'
+    # env_id = 'CartPole-v1'
     n_threads = 2
     n_actions = 4
     # n_actions = 2

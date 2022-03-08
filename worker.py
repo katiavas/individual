ef205af67f1ed859a202c166b8ee406d7c3282ce
katiavas <bl19548@bristol.ac.uk>
2022-03-08 15:12:34 +0000

    test

--- worker.py
+++ worker.py
@@ -99,6 +99,7 @@ def worker(name, input_shape, n_actions, global_agent, global_icm,
             # print("input img worker render", input_img.shape)
             input_img = resize(input_img, (3, 84, 84)) # Resize for cartPole
             input_img = input_img.transpose((2, 0, 1))
+            #
 
             state = T.tensor([input_img], dtype=T.float)
             # print("state/input img shape in worker", state.shape)

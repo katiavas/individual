f001cecab09ff193b881534d32fb49f4028bff49
katiavas <bl19548@bristol.ac.uk>
2022-03-08 20:00:49 +0000

    test

--- worker.py
+++ worker.py
@@ -97,8 +97,9 @@ def worker(name, input_shape, n_actions, global_agent, global_icm,
             # input_img = input_img.transpose((0, 1, 2))
             # input_img = get_image(env)
             # print("input img worker render", input_img.shape)
-            # input_img = resize(input_img, (3, 84, 84)) # Resize for b
-            input_img = input_img.transpose((2, 0, 1))
+            input_img = resize(input_img, (3, 84, 84)) # Resize for b
+            # input_img = input_img.transpose((2, 0, 1))
+            input_img = input_img.transpose((0, 1, 2))
             state = T.tensor([input_img], dtype=T.float)
             # print("state/input img shape in worker", state.shape)
             # feed forward our state and our hidden state to the local agent to get the action we want to take,
@@ -115,8 +116,9 @@ def worker(name, input_shape, n_actions, global_agent, global_icm,
             reward = 0  # turn off extrinsic rewards
             memory.remember(obs, action, reward, obs_, value, log_prob)
             obs = obs_
+            obs = resize(obs, (3, 84, 84))
             # print(obs.shape)
-            obs = obs.transpose((2, 0, 1))
+            obs = obs.transpose((0, 1, 2))
             # print(obs.shape)
             obs = T.tensor([obs], dtype=T.float)
             # shape of obs: (4,)

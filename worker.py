7a84f4eec4bb0d22518a717b01a0d5b78f7ed25d
katiavas <bl19548@bristol.ac.uk>
2022-03-09 12:31:54 +0000

    test

--- worker.py
+++ worker.py
@@ -97,13 +97,11 @@ def worker(name, input_shape, n_actions, global_agent, global_icm,
             input_img = env.render(mode='rgb_array')
             # input_img = resize(input_img, (3, 240, 160)) # Resize for cartPole
             # input_img = input_img.transpose((0, 1, 2))
-            # input_img = get_image(env)
             # print("input img worker render", input_img.shape)
             input_img = resize(input_img, (3, 84, 84)) # Resize for breakout
             # input_img = input_img.transpose((2, 0, 1))
             input_img = input_img.transpose((0, 1, 2))
             state = T.tensor([input_img], dtype=T.float)
-            # print("state/input img shape in worker", state.shape)
             # feed forward our state and our hidden state to the local agent to get the action we want to take,
             # value for that state, log_prob for that action
             action, value, log_prob, hx = local_agent(state, hx)

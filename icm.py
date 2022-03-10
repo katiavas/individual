063f54e5ea85d6ec7f35bd9ed9a45ae33900acac
katiavas <bl19548@bristol.ac.uk>
2022-03-10 12:42:00 +0000

    test

--- icm.py
+++ icm.py
@@ -80,11 +80,12 @@ class ICM(nn.Module):
         """ We have to concatenate a state and action and pass it through the inverse layer """
         "and activate it with an elu activation--> exponential linear"
         obs = T.Tensor(obs)
-        # obs = obs.view(obs.size()[0], -1).to(T.float)
-        # new_obs = new_obs.view(new_obs.size()[0], -1).to(T.float)
         state = self.encoder(obs)
         with T.no_grad():
             new_state = self.encoder(new_obs)
+
+        state = state.view(state.size()[0], -1).to(T.float)
+        new_state = new_state.view(new_state.size()[0], -1).to(T.float)
 
         print(state.shape)
         # Create inverse layer
--- workers.py
+++ workers.py
@@ -1,27 +1,7 @@
-'''import collections
 import cv2
 import numpy as np
 import gym
 
-
-class RepeatAction(gym.Wrapper):
-    def __init__(self, env=None, repeat=4, fire_first= False):
-        super(RepeatAction, self).__init__()
-        self.repeat = repeat
-        self.shape = env.observation_space.low.shape
-        self.fire_first = fire_first
-
-    def step(step, action):
-        t_reward = 0.0
-        done = False
-        for i in range(self.repeat):
-            obs, reward, done, info ='''
-
-
-import collections
-import cv2
-import numpy as np
-import gym
 
 class PreprocessFrame(gym.ObservationWrapper):
     def __init__(self, shape, env=None):

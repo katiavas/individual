b13c3abebd9e155ccd8425cca3a9c6c840845505
katiavas <bl19548@bristol.ac.uk>
2022-03-10 15:33:05 +0000

    test

--- worker.py
+++ worker.py
@@ -10,7 +10,6 @@ import torch as T
 import cv2
 # from utils import plot_learning_curve_with_shaded_error
 import matplotlib.pyplot as plt
-from workers import make_env
 
 from skimage.transform import resize
 
@@ -20,12 +19,8 @@ def downscale_obs(obs, new_size=(42, 42), to_gray=True):
         return resize(obs, new_size, anti_aliasing=True).max(axis=2)
     else:
         return resize(obs, new_size, anti_aliasing=True)
-
-
 def prepare_state(state):
     return T.from_numpy(downscale_obs(state, to_gray=True)).float().unsqueeze(dim=0)
-
-
 def prepare_multi_state(state1, state2):
     state1 = state1.clone()
     tmp = T.from_numpy(downscale_obs(state2, to_gray=True)).float()
@@ -33,8 +28,6 @@ def prepare_multi_state(state1, state2):
     state1[0][1] = state1[0][2]
     state1[0][2] = tmp
     return state1
-
-
 def prepare_initial_state(state, N=3):
     state_ = T.from_numpy(downscale_obs(state, to_gray=True)).float()
     tmp = state_.repeat((N, 1, 1))
@@ -49,23 +42,12 @@ def get_image(env):
     print(img.shape)
     # convert an image from one colour space to another(from rgb to gray)
     img_rgb = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    img_rgb_resized = cv2.resize(img_rgb, (84, 84), interpolation=cv2.INTER_CUBIC)
+    img_rgb_resized = cv2.resize(img_rgb, (240, 160), interpolation=cv2.INTER_CUBIC)
     # make all pixels black
     # img_rgb_resized[img_rgb_resized < 255] = 0
     # make pixel values between 0 and 1
     img_rgb_resized = img_rgb_resized / 255
     return img_rgb_resized
-
-
-def get_img(env, input_shape):
-    obs = env.render(mode='rgb_array')
-    new_frame = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
-    resized_screen = resize(new_frame, (84, 84))
-    # self.shape will be either 1 for grayscale or 3 for coloured image
-    new_obs = np.array(resized_screen, dtype=np.uint8).reshape(input_shape)
-    # make pixel values between 0 and 1
-    new_obs = new_obs / 255.0
-    return new_obs
 
 
 def get_screen(env):
@@ -78,54 +60,86 @@ def worker(name, input_shape, n_actions, global_agent, global_icm,
            optimizer, icm_optimizer, env_id, n_threads, icm=False):
     T.manual_seed(5)
     T_MAX = 20
-
     local_agent = ActorCritic(input_shape, n_actions)
 
     if icm:
         local_icm = ICM(input_shape, n_actions)
+        # just a string for printing debug information to the terminal && saving our plot
+        algo = 'ICM'
     else:
-        local_icm = None
-        intrinsic_reward = None
-
+        intrinsic_reward = T.zeros(1)
+        algo = 'A3C'
+    # each agent gets its own memory
     memory = Memory()
+    # its own environment
     env = gym.make(env_id)
-
-    # img_shape = [input_shape[1], input_shape[2], 1]
-    # env = make_env(env_id, shape=img_shape)
-
-    episode, max_steps, t_steps, scores = 0, 1000, 0, []
-
-    while episode < max_steps:
+    # how many time steps we have, the episode , the score, the average score
+    t_steps, max_eps, episode, scores, avg_score = 0, 1000, 0, [], 0
+    # We have 1000 episodes/ time steps
+    intr = []
+    while episode < max_eps:
+        # at the top of every episode reset the environment
         obs = env.reset()
-        score, done, ep_steps = 0, False, 0
+        obs = T.tensor([obs], dtype=T.float)
+        # print("obs worker", obs.dtype())
+        # make your hidden state for the actor critic a3c
         hx = T.zeros(1, 256)
+        # we need a score, a terminal flag and the number of steps taken withing the episode
+        # every 20 steps in an episode we want to execute the learning function
+        # input_img = input_img.transpose((2, 0, 1))
+        score, done, ep_steps = 0, False, 0
         while not done:
-            obs = get_img(env, input_shape)
-            state = T.tensor([obs], dtype=T.float)
+            # state = T.tensor([obs], dtype=T.float)
+            input_img = env.render(mode='rgb_array')
+            # input_img = resize(input_img, (3, 240, 160)) # Resize for cartPole
+            # input_img = input_img.transpose((0, 1, 2)) # numpy expects images to be first and channels last so we have to transpose?
+            # print("input img worker render", input_img.shape)
+            input_img = resize(input_img, (3, 84, 84)) # Resize for breakout
+            input_img = input_img.transpose((0, 1, 2))
+            state = T.tensor([input_img], dtype=T.float)
+            # feed forward our state and our hidden state to the local agent to get the action we want to take,
+            # value for that state, log_prob for that action
             action, value, log_prob, hx = local_agent(state, hx)
+            # shape of input_img: (400,600,3) ... after resize:(240,160,3)
+            # observation represents environments next state
+            # take your action for each step of the episode
             obs_, reward, done, info = env.step(action)
-            memory.remember(obs, action, obs_, reward, value, log_prob)
-            score += reward
-            obs = obs_
-            ep_steps += 1
+            # increment total steps, episode steps, increase your score
             t_steps += 1
+            ep_steps += 1
+            score += reward
+            reward = 0  # turn off extrinsic rewards
+            memory.remember(obs, action, reward, obs_, value, log_prob)
+            obs = obs_
+            obs = resize(obs, (3, 84, 84))
+            obs = obs.transpose((0, 1, 2))
+            obs = T.tensor([obs], dtype=T.float)
+            # print("obs", obs)
+            # shape of obs: (4,)
+            # LEARNING
+            # every 20 steps or when the game is done
             if ep_steps % T_MAX == 0 or done:
-                states, actions, new_states, rewards, values, log_probs = \
+                states, actions, rewards, new_states, values, log_probs = \
                     memory.sample_memory()
+                # If we are doing icm then we want to calculate our loss according to icm
                 if icm:
                     intrinsic_reward, L_I, L_F = \
                         local_icm.calc_loss(states, new_states, actions)
 
-                loss = local_agent.calc_loss(obs, hx, done, rewards,
-                                             values, log_probs,
-                                             intrinsic_reward)
+                # loss according to our a3c agent
+                loss = local_agent.calc_loss(obs, hx, done, rewards, values,
+                                             log_probs, intrinsic_reward)
+
                 optimizer.zero_grad()
                 hx = hx.detach_()
                 if icm:
                     icm_optimizer.zero_grad()
                     (L_I + L_F).backward()
+
+                """Update the weights of the network given a training sample. """
                 loss.backward()
                 T.nn.utils.clip_grad_norm_(local_agent.parameters(), 40)
+
                 for local_param, global_param in zip(
                         local_agent.parameters(),
                         global_agent.parameters()):
@@ -140,20 +154,30 @@ def worker(name, input_shape, n_actions, global_agent, global_icm,
                         global_param._grad = local_param.grad
                     icm_optimizer.step()
                     local_icm.load_state_dict(global_icm.state_dict())
-
                 memory.clear_memory()
-        episode += 1
-        # with global_idx.get_lock():
-        #    global_idx.value += 1
+        # at every episode
+        # for thread 1
         if name == '1':
+            a = T.sum(intrinsic_reward)
+            intr.append(a.detach().numpy())  # for plotting intrinsic reward
+            # env.render()  # Render environment/ visualise
             scores.append(score)
             avg_score = np.mean(scores[-100:])
-            avg_score_5000 = np.mean(scores[max(0, episode - 5000): episode + 1])
-            print('ICM episode {} thread {} of {} steps {:.2f}M score {:.2f} '
-                  'avg score (100) (5000) {:.2f} {:.2f}'.format(
-                episode, name, n_threads,
+            print('{} episode {} thread {} of {} steps {:.2f}M score {:.2f} '
+                  'intrinsic_reward {:.2f} avg score (100) {:.1f}'.format(
+                algo, episode, name, n_threads,
                 t_steps / 1e6, score,
-                avg_score, avg_score_5000))
+                T.sum(intrinsic_reward),
+                avg_score))
+
+        # end of one time step / episode
+        episode += 1
+    # At the end of the 1000 episodes
     if name == '1':
+        # print(intr)
         x = [z for z in range(episode)]
-        plot_learning_curve(x, scores, 'ICM_hallway_final.png')
+        fname = algo + '_BreakOut_no_rewards_.png'
+        # fname1 = algo + '_CartPole_intrinsic_reward1'
+        plot_learning_curve(x, scores, fname)
+        # plot_intrinsic_reward(x, intr, fname1)
+        # plot_learning_curve_with_shaded_error(x, scores, fname)

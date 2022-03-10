29cc7a2540db824ffb8cd23996d4916321306e74
katiavas <bl19548@bristol.ac.uk>
2022-03-10 15:50:19 +0000

    test

--- workers.py
+++ workers.py
@@ -8,15 +8,15 @@ class InputImg(gym.ObservationWrapper):
         # call super constructor with the environment as an input
         super(InputImg, self).__init__(env)
         self.shape = (shape[2], shape[0], shape[1])
+        """" Extract space dimensions """
         self.observation_space = gym.spaces.Box(low=0.0, high=1.0,
                                                 shape=self.shape,
                                                 dtype=np.float32)
         print(self.observation_space)
 
-    # self.shape[1:] = 84,84
     def observation(self, obs):
         input_img = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
-        img_resised = cv2.resize(input_img, self.shape[1:], interpolation=cv2.INTER_AREA)
+        img_resised = cv2.resize(input_img, self.shape[1:], interpolation=cv2.INTER_AREA)  # self.shape[1:] = 84,84
         # self.shape will be either 1 for grayscale or 3 for coloured image
         new_obs = np.array(img_resised, dtype=np.uint8).reshape((1, 84, 84))
         # make pixel values between 0 and 1

ce853fc4eb335af094b231591ac9ee2ec133fc59
katiavas <bl19548@bristol.ac.uk>
2022-03-10 12:22:28 +0000

    test

--- icm.py
+++ icm.py
@@ -79,6 +79,7 @@ class ICM(nn.Module):
         """ We have to concatenate a state and action and pass it through the inverse layer """
         "and activate it with an elu activation--> exponential linear"
         # print("icm forward observations", obs)
+        obs = T.tensor(obs)
         state = self.encoder(obs)
         with T.no_grad():
             new_state = self.encoder(new_obs)

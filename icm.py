f2669e9185ae00fd4b998d3c934ae0a7463df08b
katiavas <bl19548@bristol.ac.uk>
2022-03-11 09:33:37 +0000

    test

--- icm.py
+++ icm.py
@@ -1,4 +1,4 @@
-import torch as T
+'''import torch as T
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
@@ -39,17 +39,17 @@ class Encoder(nn.Module):
         # print('put this shape into the fc1 layer: ', enc_flatten.size())
         # Bc fc1 needs a linear input
         print(enc.shape, "enc")
-        enc_flatten = enc.view(1, -1)
+        enc_flatten = enc.view(enc.size()[0], -1)
         features = self.fc1(enc_flatten)
         print(features.shape, "features")
         # output of our cnn/ feature representation
         return features
 
 
-'''In the inverse model you want to predict the action the agent took to cause this state to transition from time t to t+1
+In the inverse model you want to predict the action the agent took to cause this state to transition from time t to t+1
 So you are comparing an integer vs an actual label/ the actual action the agent took
 Multi-class classification problem
-This is a cross entropy loss between the predicted action and the actual action the agent took'''
+This is a cross entropy loss between the predicted action and the actual action the agent took
 "The loss for the forward model is the mse between the predicted state at time t+1 and the actua state at time t+1  "
 "So we have two losses : one that comes from the inverse model and one that comes from the forward model "
 
@@ -85,8 +85,8 @@ class ICM(nn.Module):
         with T.no_grad():
             new_state = self.encoder.forward(new_obs)
 
-        state = state.view(state.size()[0], -1).to(T.float)
-        new_state = new_state.view(new_state.size()[0], -1).to(T.float)
+        # state = state.view(state.size()[0], -1).to(T.float)
+        # new_state = new_state.view(new_state.size()[0], -1).to(T.float)
         # print(new_state.shape, "new")
         # Create inverse layer
         inverse = F.elu(self.inverse(T.cat([state, new_state], dim=1)))
@@ -119,5 +119,73 @@ class ICM(nn.Module):
         # because the curiosity reward is associated with each state, so you have to take the mean across that first
         # dimension which is the number of states
         intrinsic_reward = self.alpha * ((state_ - new_state).pow(2)).mean(dim=1)
-        return intrinsic_reward, L_I, L_F
+        return intrinsic_reward, L_I, L_F'''
 
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ICM(nn.Module):
+    def __init__(self, input_dims, n_actions=3, alpha=0.1, beta=0.2):
+        super(ICM, self).__init__()
+        self.alpha = alpha
+        self.beta = beta
+
+        self.conv1 = nn.Conv2d(input_dims[0], 32, (1, 1))
+        self.conv2 = nn.Conv2d(32, 32, (1,1))
+        self.conv3 = nn.Conv2d(32, 32, (1, 1))
+        self.phi = nn.Conv2d(32, 32, (1, 1))
+
+        self.inverse = nn.Linear(64*2, 256)
+        self.pi_logits = nn.Linear(256, n_actions)
+
+        self.dense1 = nn.Linear(64+1, 256)
+        self.phi_hat_new = nn.Linear(256, 64)
+
+        device = T.device('cpu')
+        self.to(device)
+
+    def forward(self, state, new_state, action):
+        conv = F.elu(self.conv1(state))
+        conv = F.elu(self.conv2(conv))
+        conv = F.elu(self.conv3(conv))
+        phi = self.phi(conv)
+
+        conv_new = F.elu(self.conv1(new_state))
+        conv_new = F.elu(self.conv2(conv_new))
+        conv_new = F.elu(self.conv3(conv_new))
+        phi_new = self.phi(conv_new)
+
+        # [T, 32, 3, 3] to [T, 288]
+        phi = phi.view(phi.size()[0], -1).to(T.float)
+        phi_new = phi_new.view(phi_new.size()[0], -1).to(T.float)
+
+        inverse = self.inverse(T.cat([phi, phi_new], dim=1))
+        pi_logits = self.pi_logits(inverse)
+
+        # from [T] to [T, 1]
+        action = action.reshape((action.size()[0], 1))
+        forward_input = T.cat([phi, action], dim=1)
+        dense = self.dense1(forward_input)
+        phi_hat_new = self.phi_hat_new(dense)
+
+        return phi_new, pi_logits, phi_hat_new
+
+    def calc_loss(self, states, new_states, actions):
+        # don't need [] b/c these are lists of states
+        states = T.tensor(states, dtype=T.float)
+        actions = T.tensor(actions, dtype=T.float)
+        new_states = T.tensor(new_states, dtype=T.float)
+
+        phi_new, pi_logits, phi_hat_new = \
+            self.forward(states, new_states, actions)
+
+        inverse_loss = nn.CrossEntropyLoss()
+        L_I = (1 - self.beta) * inverse_loss(pi_logits, actions.to(T.long))
+
+        forward_loss = nn.MSELoss()
+        L_F = self.beta * forward_loss(phi_hat_new, phi_new)
+
+        intrinsic_reward = self.alpha*0.5*((phi_hat_new-phi_new).pow(2)).mean(dim=1)
+        return intrinsic_reward, L_I, L_F
